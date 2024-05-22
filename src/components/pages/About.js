06c4054cac176413b793786c9b1cc751f21013ce
Aman <amanprajapati7266@gmail.com>
2024-05-22 12:00:51 +0530

    add

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,12 +2,34 @@ import React from 'react';
 
 function Aboutus() {
     return (
-        <div className="container">
-            <div className="card mt-  4">
-                <div className='card-body'>
-                    <h2>About pages</h2>
+        <div>
+            <section className='py-4 bg-info'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4 my-auto">
+                            <h4>About Us</h4>
+                        </div>
+                        <div className="col-md-8 my auto">
+                            <h6 className="float-end">
+                                Home / About Us
+                            </h6>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </section>
+
+            <section className="section bg-c-light boder-bottom">
+                <div className="container">
+                    <h5 className="main-heading">Our Company</h5>
+                    <div className="underline"></div>
+                    <p className="text-white">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+                </div>
+
+            </section>
         </div>
     );
 };
