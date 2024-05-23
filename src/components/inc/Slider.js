12b0147df2fc05752bc5a7a89f71787cd0ea1010
Aman <amanprajapati7266@gmail.com>
2024-05-23 14:28:23 +0530

    add

--- src/components/inc/Slider.js
+++ src/components/inc/Slider.js
@@ -1,38 +1,48 @@
 import React from "react";
-import slider2 from '../images/slider2.jpeg';
-import slider from '../images/slider.jpg';
-import slider1 from '../images/slider1.jpeg';
+import img0 from '../images/img0.jpg';
+import img5 from '../images/img5.jpg';
+import img6 from '../images/img6.jpg';
+import img7 from '../images/img7.jpg';
+import img8 from '../images/img8.jpg';
+import '../../App.css';
 
 
-function Slider(){
+
+function Slider() {
     return (
-        <div id="carouselExampleIndicators" class="carousel slide">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src={slider2} class="d-block w-100" alt="..." />
+            <div className="carousel-inner">
+                <div className="carousel-item">
+                    <img src={img0} className="d-block w-100 custom-height" alt="..." />
                 </div>
-                <div class="carousel-item">
-                    <img src={slider} class="d-block w-100" alt="..." />
+                <div className="carousel-item active">
+                    <img src={img5} className="d-block w-100 custom-height" alt="..." />
                 </div>
-                <div class="carousel-item">
-                    <img src={slider1} class="d-block w-100" alt="..." />
+                <div className="carousel-item">
+                    <img src={img6} className="d-block w-100 custom-height" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src={img7} className="d-block w-100 custom-height" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src={img8} className="d-block w-100 custom-height" alt="..." />
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
             </button>
         </div>
     );
-};
+}
 
 export default Slider;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Slider from '../inc/Slider';
 import { Link } from 'react-router-dom';
 import Vmc from './inc/Vmc.js';
-import Servies1 from'../images/img.jpg';
+import Servies1 from '../images/img 1.jpg';
+import Servies2 from '../images/img 2.jpg';
+import Servies3 from '../images/img 4.jpg';
 
 function Home() {
     return (
@@ -43,12 +45,13 @@ function Home() {
                             <div className="card shadow">
                                 <img src={Servies1} className="w-100 boder-bottom" alt='servieses' />
                                 <div className="card-body">
-                                    <h6>Servise 1</h6>
+                                    <h6>Allahabad</h6>
                                     <div className="underline"></div>
                                     <p>
-                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                        The Allahabad Kumbh Mela, now known as the Prayagraj Kumbh Mela, is one of the largest and most significant Hindu religious festivals in the world.
+                                        Held every twelve years in Prayagraj (formerly Allahabad), India, it attracts millions of pilgrims who gather
+                                        to bathe at the confluence of the Ganges, Yamuna, and the mythical Saraswati rivers. This sacred dip is believed to
+                                        cleanse sins and bring salvation.
                                     </p>
                                     <Link to="/Servise" className="btn btn-link">Read More</Link>
                                 </div>
@@ -58,14 +61,15 @@ function Home() {
 
                         <div className="col-md-4 ">
                             <div className="card shadow">
-                                <img src={Servies1} className="w-100 boder-bottom" alt='servieses' />
+                                <img src={Servies2} className="w-100 boder-bottom" alt='servieses' />
                                 <div className="card-body">
-                                    <h6>Servise 1</h6>
+                                    <h6> Varanasi</h6>
                                     <div className="underline"></div>
                                     <p>
-                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                    The Ganga Aarti in Varanasi, also known as Banaras, is a captivating and revered Hindu ritual dedicated to the River Ganges.
+                                     Conducted daily at the Dashashwamedh Ghat during the evening, this spiritual ceremony involves a series of devotional practices
+                                      performed by priests. They use large fire lamps, incense, and rhythmic chants to honor the river, believed to be a goddess in Hinduism.
+
                                     </p>
                                     <Link to="/Servise" className="btn btn-link">Read More</Link>
                                 </div>
@@ -74,14 +78,15 @@ function Home() {
 
                         <div className="col-md-4 ">
                             <div className="card shadow">
-                                <img src={Servies1} className="w-100 boder-bottom" alt='servieses' />
+                                <img src={Servies3} className="w-100 boder-bottom" alt='servieses' />
                                 <div className="card-body">
-                                    <h6>Servise 1</h6>
+                                    <h6>Jaunpur</h6>
                                     <div className="underline"></div>
                                     <p>
-                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                    Shahi Qila, also known as Jaunpur Fort, is a historic fortress located in Jaunpur, Uttar Pradesh, India. Built in the 14th century by Firoz Shah Tughlaq,
+                                    the fort stands on the banks of the Gomti River. It is renowned for its impressive architecture, including massive gates, ornate balconies, and intricate
+                                    carvings that reflect a blend of Indo-Islamic styles. The Shahi Qila served as a stronghold for the Sharqi dynasty.
+
                                     </p>
                                     <Link to="/Servise" className="btn btn-link">Read More</Link>
                                 </div>
